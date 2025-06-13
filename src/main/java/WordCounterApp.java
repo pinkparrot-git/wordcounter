0ab1e0d0fac0854fff12e12cbc20b106c804736d
pinkparrot-git <m2.raei30@gmail.com>
2025-06-13 16:21:33 +0200

    1-Add FileUtilService class to read the stopwords list for filtering counted words; 2-Add unit tests for FileUtilService; 3-Update README with stopwords details and usage instructions

--- src/main/java/WordCounterApp.java
+++ src/main/java/WordCounterApp.java
@@ -1,21 +1,29 @@
+import java.io.IOException;
 import java.util.Scanner;
 
 public class WordCounterApp {
     public static void main(String[] args) {
-        System.out.println("Welcome to WordCounterApp! \n" +
-                "Type a sentence and I'll count the *real* words for you.\n" +
-                "Type 'exit' to make me stop counting.\n");
-        Scanner scanner = new Scanner(System.in);
-        WordCounterService wordCounter = new WordCounterService();
-        while (true) {
-            System.out.print("Enter text: ");
-            String input = scanner.nextLine();
-            if ("exit".equalsIgnoreCase(input.trim())) {
+        try {
+            FileUtilService fileUtilService = new FileUtilService();
+            WordCounterService wordCounter = new WordCounterService(fileUtilService);
+
+            System.out.println("Welcome to WordCounterApp! \n" +
+                    "Type a sentence and I'll count the *real* words for you.\n" +
+                    "Type 'exit' to make me stop counting.\n");
+            Scanner scanner = new Scanner(System.in);
+
+            while (true) {
+                System.out.print("Enter text: ");
+                String input = scanner.nextLine();
+                if ("exit".equalsIgnoreCase(input.trim())) {
                     System.out.println("See you soon!");
                     break;
+                }
+                int wordCount = wordCounter.countValidWords(input);
+                System.out.println("Number of words: " + wordCount);
             }
-            int wordCount = wordCounter.countValidWords(input);
-            System.out.println("Number of words: " + wordCount);
+        } catch (IOException e) {
+            System.err.println("Failed to initialize WordCounterApp: " + e.getMessage());
         }
     }
 }
