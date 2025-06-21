e49a3fab110ccb61259fc0eb88f1444576479757
pinkparrot-git <m2.raei30@gmail.com>
2025-06-21 12:43:26 +0200

    remove WordCounterFactory class + use better naming convention for services

--- src/main/java/service/IWordCounterService.java
+++
@@ -1,7 +0,0 @@
-package service;
-
-public interface IWordCounterService {
-
-    int countValidWords(String input);
-
-}
---
+++ src/main/java/service/WordCounterServiceImpl.java
@@ -0,0 +1,59 @@
+package service;
+
+import configuration.WordCounterConfiguration;
+
+import java.util.Arrays;
+import java.util.Set;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class WordCounterServiceImpl implements WordCounterService {
+    private final FileService fileUtilService;
+    private final String regex;
+    private final boolean caseSensitive;
+    private final String stopWordsFilePath;
+    private Set<String> stopWords;
+
+    public WordCounterServiceImpl(FileService fileUtilService, WordCounterConfiguration configuration) {
+        this.fileUtilService = fileUtilService;
+        this.regex = configuration.getWordPattern();
+        this.caseSensitive = configuration.isCaseSensitive();
+        this.stopWordsFilePath = configuration.getStopWordsFilePath();
+    }
+
+    public int countValidWords(String input) {
+        if (input == null || input.isBlank()) {
+            return 0;
+        }
+        int count = 0;
+        String[] words = input.trim().split("\\s+");
+        for (String word : words) {
+            if (isRealWord(word) && !isStopWord(word)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    private boolean isRealWord(String word) {
+        return Pattern.matches(regex, word);
+    }
+
+    private boolean isStopWord(String word) {
+        return stopWords.contains(caseSensitive ? word : word.toLowerCase());
+    }
+
+    private Set<String> normalizeStopWords() {
+        String fileContent = fileUtilService.loadTextFromFile(stopWordsFilePath);
+        Stream<String> words = Arrays.stream(fileContent.trim().split("\\s+"));
+        if (!caseSensitive) {
+            words = words.map(String::toLowerCase);
+        }
+        return words.collect(Collectors.toSet());
+    }
+
+    public void initialize() {
+        this.stopWords = normalizeStopWords();
+    }
+}
